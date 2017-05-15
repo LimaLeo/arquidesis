7f02c790eecabc2b301a06fbf79aae155b9178a6
Leonardo Lima <leonardotlima2@gmail.com>
2017-05-14 22:30:25 -0300

    Add controller CRUD update + delete

--- ControlePredialServlet/src/br/usjt/arqdesis/command/ExcluirUsuario.java
+++ ControlePredialServlet/src/br/usjt/arqdesis/command/ExcluirUsuario.java
@@ -2,17 +2,35 @@ package br.usjt.arqdesis.command;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import br.usjt.arqdesis.model.Usuario;
+import br.usjt.arqdesis.service.UsuarioService;
 
 public class ExcluirUsuario implements Command {
 
 	@Override
 	public void executar(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-
+		int pIdUsuario = Integer.parseInt(request.getParameter("id-usuario"));
+        
+        //instanciar o javabean
+        Usuario usuario = new Usuario();
+        
+        //instanciar o service
+        UsuarioService us = new UsuarioService();
+        usuario = us.carregar(pIdUsuario);
+        us.excluir(usuario.getIdUsuario());
+        
+        //enviar para o jsp
+        request.setAttribute("usuario", usuario);
+        
+        RequestDispatcher view = 
+        request.getRequestDispatcher("Usuario.jsp");
+        view.forward(request, response);	
 	}
 
 }
